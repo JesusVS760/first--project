1f0f022a8e361b3fdc50069f93fb306fbba5f716
JesusVA760 <tonio4340406@gmail.com>
2024-01-09 15:46:34 -0800

    progress

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import "./CSS/Todo.css";
+import { useRef } from "react";
+
+let count = 0;
 
 export const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+  const add = () => {
+    setTodos([
+      ...todos,
+      { no: count++, text: inputRef.current.value, display: "" },
+    ]);
+
+    inputRef.current.value = "";
+  };
+
   return (
     <div className="todo">
       <div className="todo-header">To-Do List</div>
       <div className="todo-add">
-        <input type="text" placeholder="Add Your Task" className="todo-input" />
-        <div className="todo-add-btn">ADD</div>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Add Your Task"
+          className="todo-input"
+        />
+        <div
+          onClick={() => {
+            add();
+          }}
+          className="todo-add-btn"
+        >
+          ADD
+        </div>
       </div>
       <div className="todo-list"></div>
     </div>
