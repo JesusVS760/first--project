805006d80f6f559d225c78c9f446329d6d3b3648
JesusVA760 <tonio4340406@gmail.com>
2024-01-10 11:40:54 -0800

    Finished

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./CSS/Todo.css";
 import { useRef } from "react";
+import React, { useEffect } from "react";
+import { TodoItems } from "./TodoItems";
 
 let count = 0;
 
@@ -14,7 +16,20 @@ export const Todo = () => {
     ]);
 
     inputRef.current.value = "";
+    localStorage.setItem("todos_count", count);
   };
+
+  useEffect(() => {
+    setTodos(JSON.parse(localStorage.getItem("todos")));
+    count = localStorage.getItem("todos_count");
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(todos);
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, 100);
+  }, [todos]);
 
   return (
     <div className="todo">
@@ -35,7 +50,19 @@ export const Todo = () => {
           ADD
         </div>
       </div>
-      <div className="todo-list"></div>
+      <div className="todo-list">
+        {todos.map((item, index) => {
+          return (
+            <TodoItems
+              key={index}
+              setTodos={setTodos}
+              no={item.no}
+              display={item.display}
+              text={item.text}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
